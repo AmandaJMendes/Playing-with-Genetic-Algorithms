52586faf86de4f5d956d6961567f9aee3fb18eaf
AmandaJMendes <amandajm2305@gmail.com>
2024-04-19 12:29:05 -0300

    Improving mutation

--- GA_functions.py
+++ GA_functions.py
@@ -4,7 +4,7 @@ def initialize(population_size, chromossome_dim, data_type, min_value, max_value
     if data_type == int:
         return np.random.randint(min_value, max_value+1, size = (population_size, chromossome_dim))
     elif data_type == float:
-        return np.random.uniform(min_value, max_value+1, size = (population_size, chromossome_dim))
+        return np.random.uniform(min_value, max_value, size = (population_size, chromossome_dim))
     else:
         raise Excpetion(f"Unsupported data type {data_type}")
 
@@ -39,35 +39,43 @@ def select(population, scores, elite_p = 0.0, random_p = 0.0, mode = "max", sele
 def normalize_fitness(scores):
     return (scores+1E-10)/(scores+1E-10).sum()
 
-def one_point_crossover(parents, mutation_prob, min_value, max_value):
+def one_point_crossover(parents):
     offspring = []
-    for i in range(0, parents.shape[0], 2):
-        childA = parents[i].copy()
-        childB = parents[i+1].copy()
+    n_couples = int(np.ceil(parents.shape[0]/2))
+    crossover_idxs = np.random.randint(1, parents.shape[1], size = n_couples)
+    couples = np.random.randint(1, parents.shape[0], size = (n_couples, 2))
 
-        crossover_idx = np.random.randint(1, parents.shape[1])
-        
-        childA[crossover_idx:] = parents[i+1][crossover_idx:]
-        childB[crossover_idx:] = parents[i][crossover_idx:]
-
-        mutate_children = np.random.uniform(size = 2) <= mutation_prob
-        if mutate_children[0]:
-            childA[np.random.randint(parents.shape[1])] = np.random.randint(min_value, max_value+1)
-        if mutate_children[1]:
-            childB[np.random.randint(parents.shape[1])] = np.random.randint(min_value, max_value+1)
-
+    for i in range(parents.shape[0]//2):
+        parentA, parentB = parents[couples[i]]
+        childA, childB   = parentA.copy(), parentB.copy()
+        childA[crossover_idxs[i]:] = parentB[crossover_idxs[i]:]
+        childB[crossover_idxs[i]:] = parentA[crossover_idxs[i]:]
         offspring += [childA, childB]
 
+    offspring = offspring[:-1] if parents.shape[0] % 2 else offspring
     return np.array(offspring)
 
+def mutate(population, mutation_prob, min_value, max_value):
+    if mutation_prob:
+        mutation_mask      = np.random.uniform(size = population.shape[0]) <= mutation_prob
+        children_to_mutate = mutation_mask.nonzero()[0]
+        gens_to_mutate     = np.random.randint(0, population.shape[1], size = len(children_to_mutate))
+
+        if population.dtype == int:
+            new_gens = np.random.randint(min_value, max_value+1, size = len(children_to_mutate))
+        else:
+            new_gens = np.random.uniform(min_value, max_value,   size = len(children_to_mutate))
+        population[children_to_mutate, gens_to_mutate] = new_gens
+    return population
 
 if __name__ == "__main__":
-    initial_population = initialize(100, 10, float, -5, 100)
+    initial_population = initialize(200, 10, float, -5, 1000)
     offspring = initial_population
-    for i in range(8000):
+    for i in range(10000):
         fitness = evaluate_fitness(offspring, 3700)
-        selected = select(offspring, fitness, 0.2, 0.1, mode = "min")
-        offspring = one_point_crossover(selected, 0.001, -5, 1000)
+        selected = select(offspring, fitness, 0.1, 0.01, mode = "min")
+        offspring = one_point_crossover(selected)
+        offspring = mutate(offspring, 0.01, -5, 1000)
     solutions = np.unique(offspring, axis = 0, return_counts = True)
     for solution, count in zip(solutions[0], solutions[1]):
         print(solution, ' | Occurences: ', count, ' | Sum: ', solution.sum())
